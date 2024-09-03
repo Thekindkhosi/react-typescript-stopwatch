12f726a622a2faccdd5bc7e1658b3056e9cadcdd
Amir Khosravi <amirkhosravi921@gmail.com>
2024-09-03 14:52:19 +0330

    Styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.scss";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <section>
+        <div className="stopwatch-container">
+          <h1>StopWatch</h1>
+          <h3>00:00:00</h3>
+          <div className="button-container">
+            <button>Start/Stop</button>
+            <button>Reset</button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
